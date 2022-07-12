2e55f9b49583a788b96794cdb426ff3cbc77084b
Shubham D <shubhamd@axioned.com>
2022-07-12 22:41:38 +0530

    rafactor: variable optimization

--- assets/js/media-button.js
+++ assets/js/media-button.js
@@ -1,33 +1,27 @@
-gsap.registerPlugin(CSSRulePlugin);
-
 const t1 = gsap.timeline({ default: {ease: Power2.easeOut}})
-const rule = CSSRulePlugin.getRule('button::before');
 
 const btn = document.querySelector('.btn');
-const l1 = document.querySelector('.media-button ul li:nth-child(1)');
-const l2 = document.querySelector('.media-button ul li:nth-child(2)');
-const l3 = document.querySelector('.media-button ul li:nth-child(3)');
-const l4 = document.querySelector('.media-button ul li:nth-child(4)');
+let listCount = document.querySelector(".media-button ul").children.length;
+let list = {};
+let svg = {};
+let svgContainer = {};
 
-const l1Svg = document.querySelector('.media-button ul li:nth-child(1) svg');
-const l2Svg = document.querySelector('.media-button ul li:nth-child(2) svg');
-const l3Svg = document.querySelector('.media-button ul li:nth-child(3) svg');
-const l4Svg = document.querySelector('.media-button ul li:nth-child(4) svg');
+for (let i = 1; i <= listCount; i++) {
+    list["l"+i] = document.querySelector(`.media-button ul li:nth-child(${i})`);
+    svgContainer["svgContainerL"+i] = document.querySelector(`.media-button ul li:nth-child(${i}) .svg-container`);
+    svg["svgL"+i] = document.querySelector(`.media-button ul li:nth-child(${i}) svg`);
+}
 
-const l4SvgContainer = document.querySelector('.media-button ul li:nth-child(4) .svg-container');
-const l3SvgContainer = document.querySelector('.media-button ul li:nth-child(3) .svg-container');
-const l2SvgContainer = document.querySelector('.media-button ul li:nth-child(2) .svg-container');
-const l1SvgContainer = document.querySelector('.media-button ul li:nth-child(1) .svg-container');
-const l4Common = {
+const listStyle = {
     borderRadius: '40px',
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
-    background: 'rgba(255,255,255,0.1)',
+    justifyContent: 'flex-start',
+    background: 'rgba(243	244	252,0.1)',
     width: '62px',
 };
 
-const svgCommon = {
+const svgStyle = {
     position: 'absolute',
     top: '50%',
     left: '50%',
@@ -36,27 +30,28 @@ const svgCommon = {
 }
 
 t1.fromTo('.btn', { rotation: 0}, { rotation: 315, duration: 1.5, ease: "power4.inOut"}, "1s")
-.to(l4, { bottom: '100%', duration: 0.5, ease: "power4.Out"}, "-=1s")
-.to(l3, { bottom: '75%', duration: 0.5, ease: "power4.Out"}, "-=0.75s")
-.to(l2, { bottom: '50%', duration: 0.5, ease: "power4.Out"}, "-=0.5s")
-.to(l1, { bottom: '25%', duration: 0.5, ease: "power4.Out"}, "-=0.25s")
-.fromTo(l4Svg, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.5s")
-.fromTo(l3Svg, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.4s")
-.fromTo(l2Svg, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.3s")
-.fromTo(l1Svg, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.2s")
-// .fromTo(l1SvgContainer, { width: '0',height: '64px' },{ width: '180px',height: '64px' })
-.fromTo(l1SvgContainer, svgCommon,{ xPercent: -160, duration: 0.20 }, "-=0.15s")
-.fromTo(l2SvgContainer, svgCommon,{ xPercent: -195, duration: 0.30 }, "-=0.20s")
-.fromTo(l3SvgContainer, svgCommon,{ xPercent: -225, duration: 0.35 }, "-=0.25s")
-.fromTo(l4SvgContainer, svgCommon,{ xPercent: -255, duration: 0.40 }, "-=0.30s") // same
+.to(list.l4, { bottom: '100%', duration: 0.5, ease: "power4.Out"}, "-=1s")
+.to(list.l3, { bottom: '75%', duration: 0.5, ease: "power4.Out"}, "-=0.75s")
+.to(list.l2, { bottom: '50%', duration: 0.5, ease: "power4.Out"}, "-=0.5s")
+.to(list.l1, { bottom: '25%', duration: 0.5, ease: "power4.Out"}, "-=0.25s")
 
-.fromTo(l1, l4Common,{ width: '180px',height: '64px',background: 'rgba(255,255,255, 1)',
+.fromTo(svg.svgL4, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.5s")
+.fromTo(svg.svgL3, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.4s")
+.fromTo(svg.svgL2, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.3s")
+.fromTo(svg.svgL1, { opacity: 0, scale: 0},{ opacity: 1, scale: 1 }, "-=0.2s")
+
+.fromTo(svgContainer.svgContainerL1, svgStyle,{ xPercent: -160, duration: 0.20 }, "-=0.15s")
+.fromTo(svgContainer.svgContainerL2, svgStyle,{ xPercent: -195, duration: 0.30 }, "-=0.20s")
+.fromTo(svgContainer.svgContainerL3, svgStyle,{ xPercent: -225, duration: 0.35 }, "-=0.25s")
+.fromTo(svgContainer.svgContainerL4, svgStyle,{ xPercent: -255, duration: 0.40 }, "-=0.30s") // same
+
+.fromTo(list.l1, listStyle,{ width: '180px',height: '64px',background: 'rgba(255,255,255, 1)',
 duration: 0.20}, "-=0.45s")
-.fromTo(l2, l4Common,{ width: '215px',height: '64px',background: 'rgba(255,255,255, 1)',
+.fromTo(list.l2, listStyle,{ width: '215px',height: '64px',background: 'rgba(255,255,255, 1)',
 duration: 0.25}, "-=0.40s")
-.fromTo(l3, l4Common,{ width: '245px',height: '64px',background: 'rgba(255,255,255, 1)',
+.fromTo(list.l3, listStyle,{ width: '245px',height: '64px',background: 'rgba(255,255,255, 1)',
 duration: 0.30}, "-=0.35s")
-.fromTo(l4, l4Common,{ width: '275px',height: '64px',background: 'rgba(255,255,255, 1)',
+.fromTo(list.l4, listStyle,{ width: '275px',height: '64px',background: 'rgba(255,255,255, 1)',
 duration: 0.40}, "-=0.30s")
 
 .fromTo(`.text`, {
@@ -64,6 +59,7 @@ duration: 0.40}, "-=0.30s")
 },{ opacity: 1,width: '100%', xPercent: 10 }, "-=0.5s")
 
 t1.pause();
+
 btn.addEventListener('click', () => {
     if (btn.classList.contains('active')) {
         t1.reverse();
