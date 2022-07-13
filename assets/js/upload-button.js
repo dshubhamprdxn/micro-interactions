c08279175a3607e7cbfe85bd9f50cffcbafe3f67
dpriyankaprdxn <priyankad@axioned.com>
2022-07-13 20:16:45 +0530

    uploading animation chnages done

--- assets/js/upload-button.js
+++ assets/js/upload-button.js
@@ -6,31 +6,13 @@ const upload = document.querySelector('.upload');
 const loader = document.querySelector('.loader');
 const completed = document.querySelector('.completed');
 
-
-const l4Common = {
-    borderRadius: '40px',
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-    background: '#fff',
-    width: '0',
-};
-
-const svgCommon = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    xPercent: -50,
-    yPercent: -50
-}
-
 t1.to('.upload', {  marginRight: 0, width: '120px' ,height: '100%', duration: 0.5})
 .to('.text', {  opacity: 0})
-.to('.bg', {  width: '100%', duration: 2.5}, 0.5)
-.to('.uploading', { y: 0, xPercent: 150 }, "-=1.5")
-.to('.loader', {width: '100%', display: 'block',duration: 2.5}, 3)
-.to('.completed', {height: '100%', visibility: 'visible', borderRadius: '10px'},5)
+.to('.bg', {  width: '100%', duration: 1.5}, 0.5)
+.to('.uploading', { visibility: 'visible', height: '50%'}, "-=0.3")
+.to('.loader', {width: '100%', display: 'block',duration: 1}, "-=0.3")
 .to('.loader', {height: '100%', inset : 0 , borderRadius: '10px'})
+.to('.completed', {visibility: 'visible', borderRadius: '10px',duration: 0.5},"-=0.2")
 
 t1.pause();
 upload.addEventListener('click', () => {
