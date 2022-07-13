d643fc950ab4abb19299d9ee557ab77eebf7ed05
Shubham D <shubhamd@axioned.com>
2022-07-13 18:38:12 +0530

    Add slideIn, slideOut animation to Message

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 let loadingSwitchOn = false;
 let loadingSwitchButtonIsDisabled = false;
 
+const displayedMessageClassName = "current-message";
 const messages = ["devices off", "please wait ...", "devices on"];
 
 const mainContainer = document.querySelector(".main-container");
@@ -10,15 +11,42 @@ const slider = switchInteractionMainDiv.querySelector(".slider");
 const fill = switchInteractionMainDiv.querySelector(".fill");
 const cover1 = switchInteractionMainDiv.querySelector(".cover-1");
 const cover2 = switchInteractionMainDiv.querySelector(".cover-2");
-const messageSpan = switchInteractionMainDiv.querySelector(".message");
+const messageDiv = switchInteractionMainDiv.querySelector(".message");
 
 const t1 = gsap.timeline();
 const t2 = gsap.timeline();
 const t3 = gsap.timeline();
 const t4 = gsap.timeline();
 
-const changeMessage = (message) => {
-    messageSpan.innerText = message;
+const addClass = (element, className) => {
+    if(!element.classList.contains(className)) {
+        element.classList.add(className);
+    }
+}
+
+const removeClass = (element, className) => {
+    if(element.classList.contains(className)) {
+        element.classList.remove(className);
+    }
+}
+
+const changeMessage = (message1ID, message2ID) => {
+    const message1Span = messageDiv.querySelector(`span:nth-child(${message1ID}`);
+    const message2Span = messageDiv.querySelector(`span:nth-child(${message2ID}`);
+
+    addClass(message2Span, displayedMessageClassName);
+
+    const t5 = gsap.timeline();
+    t5.pause();
+    if(message2ID > message1ID) {
+        t5.fromTo(message1Span, {opacity: 1}, {opacity: 0, duration: 0.5, y: -20, ease: "none"})
+        .fromTo(message2Span, {opacity: 0, y: 20}, {opacity: 1, duration: 0.5, y: 0, ease: "none", onComplete: removeClass, onCompleteParams: [message1Span, displayedMessageClassName]}, "<");
+    } else {
+        t5.fromTo(message1Span, {opacity: 1}, {opacity: 0, duration: 0.5, y: 20, ease: "none"})
+        .fromTo(message2Span, {opacity: 0, y: -20}, {opacity: 1, duration: 0.5, y: 0, ease: "none", onComplete: removeClass, onCompleteParams: [message1Span, displayedMessageClassName]}, "<");
+    }
+    
+    t5.restart();
 }
 
 const changeSwitchValue = () => {
@@ -41,8 +69,8 @@ const playT4 = () => {
 }
 
 t1.pause();
-t1.to(switchHTMLElement, {width: "80px", duration: 0.5, onComplete: changeMessage, onCompleteParams: [messages[1]], onReverseComplete: changeSwitchValue})
-    .to(switchHTMLElement, {backgroundColor: "grey", ease: "linear", duration: 0.5, onReverseComplete: changeMessage, onReverseCompleteParams: [messages[0]]}, "<")
+t1.to(switchHTMLElement, {width: "80px", duration: 0.5, onComplete: changeMessage, onCompleteParams: [1, 2], onReverseComplete: changeSwitchValue})
+    .to(switchHTMLElement, {backgroundColor: "grey", ease: "linear", duration: 0.5, onReverseComplete: changeMessage, onReverseCompleteParams: [2, 1]}, "<")
     .to(switchInteractionMainDiv, {backgroundColor: "lightblue", ease: "linear", duration: 0.5, onComplete: playT2}, "<")
     .set(fill, {border: ""})
     .set(fill, {borderTopColor: "transparent"});
@@ -58,9 +86,9 @@ t2.set(fill, {border: "6px solid grey", borderRadius: "50%", delay: 0.5})
 t3.pause();
 t3.set(fill, {border: ""})
     .set(fill, {borderTopColor: "transparent"})
-    .to(switchHTMLElement, {width: "150px", duration: 0.5, onComplete: changeMessage, onCompleteParams: [messages[2]], onReverseComplete: playT4})
+    .to(switchHTMLElement, {width: "150px", duration: 0.5, onComplete: changeMessage, onCompleteParams: [2, 3], onReverseComplete: playT4})
     .to(switchInteractionMainDiv, {backgroundColor: "lightgreen", ease: "linear", duration: 0.5}, "<")
-    .to(switchHTMLElement, {backgroundColor: "green", ease: "linear", duration: 0.5, onComplete: changeSwitchValue, onReverseComplete: changeMessage, onReverseCompleteParams: [messages[1]]}, "<");
+    .to(switchHTMLElement, {backgroundColor: "green", ease: "linear", duration: 0.5, onComplete: changeSwitchValue, onReverseComplete: changeMessage, onReverseCompleteParams: [3, 2]}, "<");
 
 t4.pause();
 t4.set(fill, {border: "6px solid grey", borderRadius: "50%", rotation: 45, delay: 0.5})
