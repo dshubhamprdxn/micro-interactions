148a6adfab3545791cca6823d8b54905402fb69b
Shubham D <shubhamd@axioned.com>
2022-07-13 15:33:56 +0530

    fix: minor fixes

--- assets/js/upload-button.js
+++ assets/js/upload-button.js
@@ -24,12 +24,14 @@ const svgCommon = {
     yPercent: -50
 }
 
-t1.to('.upload', {  margin: 0 ,height: '100%' , duration: 0.5})
-.to('.upload', {  width:'100%' , duration: 1.25})
-.to('.file-name', {display: 'none', duration: 0})
-.to('.loader', {width: '100%', display: 'block', duration: 2})
-.to('.completed', {height: '100%', visibility: 'visible', duration: 0})
-.to('.loader', {height: '100%', inset : '0 0 0 0' , duration: 2.5})
+t1.to('.upload', {  marginRight: 0, width: '120px' ,height: '100%', duration: 0.5})
+.to('.text', {  opacity: 0})
+.to('.bg', {  width: '100%', duration: 2.5}, 0.5)
+.to('.uploading', { y: 0, xPercent: 150 }, "-=1.5")
+// .to('.file-name', {opacity: 0,width: 0}, "-1.25s")
+// .to('.loader', {width: '100%', display: 'block', duration: 2})
+// .to('.completed', {height: '100%', visibility: 'visible', duration: 0})
+// .to('.loader', {height: '100%', inset : '0 0 0 0' , duration: 2.5})
 
 t1.pause();
 upload.addEventListener('click', () => {
