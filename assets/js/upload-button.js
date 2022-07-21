998f276856c54e29a423ddc850418107996099b9
dpriyankaprdxn <priyankad@axioned.com>
2022-07-21 13:29:30 +0530

    attachment svg added

--- assets/js/upload-button.js
+++ assets/js/upload-button.js
@@ -1,20 +1,32 @@
+const addClass = (element,className) => { 
+    console.log(drawn)
+    if(!element.classList.contains(className)) {
+        element.classList.add(className);
+    }
+}
+
 gsap.registerPlugin(CSSRulePlugin);
 
 const t1 = gsap.timeline({ default: {ease: Power2.easeOut}})
-
+const loadingTime = 5;
 const upload = document.querySelector('.upload');
 const loader = document.querySelector('.loader');
 const completed = document.querySelector('.completed');
+const trigger = document.querySelector('.trigger');
+const drawn = "drawn";
+t1.pause();
 
 t1.to('.upload', {  marginRight: 0, width: '120px' ,height: '100%', duration: 0.5})
 .to('.text', {  opacity: 0})
 .to('.bg', {  width: '100%', duration: 1.5}, 0.5)
 .to('.uploading', { visibility: 'visible', height: '50%'}, "-=0.3")
-.to('.loader', {width: '100%', display: 'block',duration: 1}, "-=0.3")
+.to('.loader', {width: '100%', display: 'block',duration: loadingTime}, "-=0.3")
 .to('.loader', {height: '100%', inset : 0 , borderRadius: '10px'})
-.to('.completed', {visibility: 'visible', borderRadius: '10px',duration: 0.5},"-=0.2")
+.to('.completed', {visibility: 'visible', borderRadius: '10px',duration: 1 ,onStart: addClass, onStartParams : [trigger,drawn]	 
+  },"-=0.1")
+// .to('.right-tik', {visibility: 'visible'},"-=0.1")
+.set(['.upload','.text','.bg','.uploading','.loader','.completed'], {clearProps: 'all'});
 
-t1.pause();
 upload.addEventListener('click', () => {
-    t1.play();
+    t1.restart();
 });
